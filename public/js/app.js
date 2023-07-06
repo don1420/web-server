d3268ae1fddb0834e6f93e9e71134d54ba2ec097
donyshchenko <donyshchenko@binaryanvil.com>
2023-07-06 19:42:51 +0300

    created form and added get data logic from the endpoint

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,25 @@ fetch('http://localhost:3000/weather?address=zaporizhia').then(response => {
         }
     })
 })
+console.log('Client side JS is loaded')
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    const weatherForm = document.querySelector('form')
+
+    weatherForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        
+        const data = Object.fromEntries(new FormData(e.target).entries());
+
+        fetch(`http://localhost:3000/weather?address=${data.location}`).then(response => {
+            response.json().then(data => {
+                if (data.error) {
+                    console.log(data.error)
+                } else {
+                    console.log(data.forecast)
+                    console.log(data.location)
+                }
+            })
+        })
+    })
+})
