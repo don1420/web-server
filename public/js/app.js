8009c5b776c84418687bc1e4b6cd315e88a62fb8
donyshchenko <donyshchenko@binaryanvil.com>
2023-07-07 14:29:50 +0300

    Added message containers

--- public/js/app.js
+++ public/js/app.js
@@ -12,19 +12,23 @@ console.log('Client side JS is loaded')
 
 document.addEventListener('DOMContentLoaded', function (event) {
     const weatherForm = document.querySelector('form')
+    const forecastContainer = document.querySelector('#forecast-container')
+    const locationContainer = document.querySelector('#location-container')
 
     weatherForm.addEventListener('submit', (e) => {
         e.preventDefault()
-        
+
         const data = Object.fromEntries(new FormData(e.target).entries());
+
+        forecastContainer.textContent = 'Loading...'
 
         fetch(`http://localhost:3000/weather?address=${data.location}`).then(response => {
             response.json().then(data => {
                 if (data.error) {
-                    console.log(data.error)
+                    forecastContainer.textContent = data.error
                 } else {
-                    console.log(data.forecast)
-                    console.log(data.location)
+                    forecastContainer.textContent = data.forecast
+                    locationContainer.textContent = data.location
                 }
             })
         })
